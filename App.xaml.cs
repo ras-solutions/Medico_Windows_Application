3774a45e85f99c665e14b9a0ed55c5e2cc58821f
Akhil Kumar <akhilskumar1999@gmail.com>
2020-04-16 23:12:36 +0530

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace Medico
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //initialize the splash screen and set it as the application main window
+            SplashScreenWindow splashScreen = new SplashScreenWindow();
+            this.MainWindow = splashScreen;
+            splashScreen.Show();
+
+            //loading on seperate thread
+            Task.Factory.StartNew(() =>
+            {
+                //simulate some work being done for now
+                System.Threading.Thread.Sleep(1700);
+
+                //since we're not on the UI thread
+                //once we're done we need to use the Dispatcher
+                //to create and show the main window
+                this.Dispatcher.Invoke(() =>
+                {
+                    //initialize the main window, set it as the application main window
+                    //and close the splash screen
+                    var loginWindow = new LoginWindow();
+                    this.MainWindow = loginWindow;
+                    //loginWindow.Opacity = 0;
+                    loginWindow.Show();
+                    /*
+                    DoubleAnimation da = new DoubleAnimation();
+                    da.From = 0;
+                    da.To = 1;
+                    da.Duration = new Duration(TimeSpan.FromSeconds(2));
+                    da.AutoReverse = true;
+                    da.RepeatBehavior = RepeatBehavior.Forever;
+                    // da.RepeatBehavior=new RepeatBehavior(3);
+                    loginWindow.BeginAnimation(OpacityProperty, da);
+                    */
+                    splashScreen.Close();
+                });
+            });
+        }
+
+    }
+}
---
+++ LoginWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Medico
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void email_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (email.Text == "Email")
+            {
+                email.Text = "";
+            }
+        }
+
+        private void email_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (email.Text == "")
+            {
+                email.Text = "Email";    
+            }
+        }
+
+        private void passwd_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (passwd.Text == "Password")
+            {
+                passwd.Text = "";
+            }
+        }
+
+        private void passwd_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (passwd.Text == "")
+            {
+                passwd.Text = "Password";
+            }
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
